61d120d4c9f6d8a67cc7053389db11e4a756e1f8
William Douglas <willstaleydouglas@gmail.com>
2026-03-16 14:55:00 -0700

    Added A* pathfinding lib and began "fish AI"
    A* is a pathfinding tool that works great for unity 2d stuff

--- Fishing Rpg/Assets/Scripts/Fish/FishManager.cs
+++ Fishing Rpg/Assets/Scripts/Fish/FishManager.cs
@@ -3,17 +3,79 @@ using UnityEngine;
 public class FishManager : MonoBehaviour
 {
     public FishObj fishData;
+
+    public enum FishState
+    {
+        Idle,
+        Lured,
+        Hooked,
+        Running
+    }
     
-    
+    public FishState currentState = FishState.Idle;
+    private Rigidbody2D rb;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        CheckBounds();
         
+        switch (currentState)
+        {
+            case FishState.Idle:
+                UpdateIdleState();
+                break;
+        }
     }
+
+    private void CheckBounds()
+    {
+        Vector3 viewportPos = Camera.main.WorldToViewportPoint(transform.position);
+        bool moved = false;
+
+        if (viewportPos.x < 0)
+        {
+            viewportPos.x = 1;
+            moved = true;
+        }
+        else if (viewportPos.x > 1)
+        {
+            viewportPos.x = 0;
+            moved = true;
+        }
+
+        if (viewportPos.y < 0)
+        {
+            viewportPos.y = 1;
+            moved = true;
+        }
+        else if (viewportPos.y > 1)
+        {
+            viewportPos.y = 0;
+            moved = true;
+        }
+
+        if (moved)
+        {
+            Vector3 worldPos = Camera.main.ViewportToWorldPoint(viewportPos);
+            worldPos.z = 0; // Maintain 2D Z position
+            transform.position = worldPos;
+        }
+    }
+
+    private void UpdateIdleState()
+    {
+        if (fishData != null && rb != null)
+        {
+            // Simple random movement for testing idle state and bounds
+            rb.linearVelocity = transform.right * fishData.speed;
+        }
+    }
+    
 }
